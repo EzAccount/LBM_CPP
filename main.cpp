a6dd5539423b8e3eaa621d0a07071f8a998f7b3f
Mikhail Tikhonov <misha7322@hotmail.com>
2019-06-30 20:01:14 +0300

    Core files initials

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "point.h"
+
+int main(int argc, char **argv) {
+    if (argc > 2){
+        sscanf(argv[1], "%zu", &iterations);
+        sscanf(argv[2], "%lf", &Kn);
+    }
+    e[0].x = 0.;     e[0].y = 0.;
+    e[1].x = 1.;     e[1].y = 0.;
+    e[2].x = 0.;     e[2].y = 1.;
+    e[3].x = -1.;    e[3].y = 0.;
+    e[4].x = 0.;     e[4].y = -1.;
+    e[5].x = 1.;     e[5].y = 1.;
+    e[6].x = -1.;    e[6].y = 1.;
+    e[7].x = -1.;    e[7].y = -1.;
+    e[8].x = 1.;     e[8].y = -1.;
+    std::vector<double> r = {1., 0.1, -0.1, 1.};
+    Point p(r);
+    p.macro();
+    std::cout<<p.tau << " " << p.rho << " " << p.T <<" " << p.v.x << " " << p.v.y << " ";
+    std::cout << std::endl;
+}
---
+++ point.h
@@ -0,0 +1,109 @@
+//
+// Created by mikhail on 8/12/18.
+//
+#ifndef LBM_CPP_POINT_H
+#define LBM_CPP_POINT_H
+
+#include <iostream>
+#include <vector>
+#include <array>
+#include "constants.h"
+
+
+class Point{
+public:
+    std::array<double, Q> f, f_temp, f_eq;
+    double tau, T, rho;
+    Vector<double> v;
+    void eq();
+    void col();
+    void macro();
+    Point();
+    Point(std::vector<double>);
+
+};
+class Grid{
+    std::vector<std::vector<Point>> points;
+    bool is_possible(int, int,int);
+    void mirroring(int,int,int);
+    void transfer(int, int);
+    void at(int, int); // Do i need thtis one?
+    void eval();
+public:
+    Grid(int, int);
+};
+
+void Point::col() {
+    for (size_t k=0; k<Q; ++k)
+    {
+        f[k] = f_temp[k] - 1./tau * (f_temp[k] -  f_eq[k]);
+    }
+}
+void Point::eq() {
+    double c = T;
+    for (size_t k=0; k<Q; ++k){
+        double sc = e[k] * v;
+        f_eq[k] = rho * w[k] * (1. + 3 * sc/c + 4.5 * sc * sc / c / c -  v * v * 1.5/ c);
+    }
+
+}
+void Point::macro() {
+    rho = 0;
+    for (size_t k=0; k<Q; ++k)
+    {
+        rho+=f_temp[k];
+    }
+    T = 0;
+    for (size_t k=0; k<Q; ++k){
+        T += (e[k] - v)*(e[k] - v) * f_temp[k] * 3/2;
+    }
+    v.x = 0;
+    v.y = 0;
+    for (size_t k=0; k<Q; ++k){
+        v += f_temp[k] / rho * e[k];
+    }
+}
+Point::Point() {
+    tau = 0.;
+    T = 0.;
+    rho = 0.;
+    v.x = 0.;
+    v.y = 0.;
+    eq();
+    for (int k=0; k<Q; ++k){
+        f[k] = f_eq[k];
+        f_temp[k] = f_eq[k];
+    }
+}
+Point::Point(std::vector<double> init){
+    tau = 0.;
+    T = init[3];
+    rho = init[0];
+    v.x = init[1];
+    v.y = init[2];
+    eq();
+    for (int k=0; k<Q; ++k){
+        f[k] = f_eq[k];
+        f_temp[k] = f_eq[k];
+    }
+}
+
+bool Grid::is_possible(int x, int y, int k) {
+    // add check for k corresponding to speed/2 [speed/3, etc]
+    if ((x + e[k].x < x_size) && (x + e[k].x >= 0) && (x + e[k].x < y_size) && (x + e[k].x >= 0)){
+        return (points[x + e[k].x][y + e[k].y].rho != -1) ;
+    }
+    return false;
+}
+void Grid::transfer(int x, int y) {
+    for (size_t k=0; k<Q; ++k){
+        if (is_possible(x,y,k)) points[x+e[k].x][y+e[k].y].f_temp[k] = points[x][y].f[k];
+    }
+}
+
+
+
+
+
+#endif //LBM_CPP_POINT_H
+
