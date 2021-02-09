2c244ee4339766ea88515a0ed8f01def7aecae55
Mikhail Tikhonov <me@mtikhonov.com>
2021-02-08 23:57:21 -0500

    Python style for data slicing, test removed from CMake

--- plots.py
+++ plots.py
@@ -1,20 +1,15 @@
+import numpy as np
 import matplotlib.pyplot as pyplot
 from mpl_toolkits.mplot3d import Axes3D
 
-
 fig = pyplot.figure()
 ax = Axes3D(fig)
-x = []
-y = []
-P = []
-f_plot = open('Test_rho.txt', 'r')
-for line in f_plot:
-    line_split = line.split()
-    x.append(float(line_split[0]))
-    y.append(float(line_split[1]))
-    P.append(float(line_split[2]))
+with open('Test_rho.txt', 'r') as results:
+    data = np.loadtxt(results)
+    x, y, rho = data.T
+
 ax.invert_xaxis()
-ax.scatter(x, y, P)
+ax.scatter(x, y, rho)
 ax.set_xlabel('x-cord')
 ax.set_ylabel('y-cord')
 ax.set_zlabel('rho')
