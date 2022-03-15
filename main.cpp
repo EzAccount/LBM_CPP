75e50bc778dc9c16a82334685b7114de698a098b
Геворкян Армен <a_gevorkyan@beorg.local>
2022-03-15 23:42:59 +0300

    new: 17 directions grid type, ref: some functions near the computing

--- main.cpp
+++ main.cpp
@@ -4,121 +4,121 @@
 #include <math.h>
 
 int main() {
-  int x_size = 100, y_size = 10;
-  std::vector<std::pair<int, int>> input_data;
-  for (size_t i = 0; i <= x_size; ++i) {
+    size_t x_size = 100, y_size = 10;
+    std::vector<std::pair<int, int>> input_data;
+    for (size_t i = 0; i <= x_size; ++i) {
+        for (size_t j = 0; j <= y_size; ++j) {
+            input_data.emplace_back(i, j);
+        }
+    }
+    Grid Pois(input_data);
+    Pois.boundaries();
+    for (size_t j = 0; j < Pois.grid[0].size(); ++j) {
+        Pois.grid[0][j].open_bound = true;
+        Pois.grid[0][j].bound = false;
+    }
+
+    for (size_t j = 0; j < Pois.grid[0].size(); ++j) {
+        Pois.grid[Pois.grid.size() - 1][j].bound = false;
+        Pois.grid[Pois.grid.size() - 1][j].open_bound = true;
+    }
+
+    Pois.weight_calculate();
     for (size_t j = 0; j <= y_size; ++j) {
-      input_data.emplace_back(i, j);
-    }
-  }
-  Grid Pois(input_data);
-  Pois.boundaries();
-  for (int j = 0; j < Pois.grid[0].size(); ++j) {
-    Pois.grid[0][j].open_bound = true;
-    Pois.grid[0][j].bound = false;
-  }
-
-  for (int j = 0; j < Pois.grid[0].size(); ++j) {
-    Pois.grid[Pois.grid.size()-1][j].bound = false;
-    Pois.grid[Pois.grid.size()-1][j].open_bound = true;
-  }
-
-  Pois.weight_calculate();
-  for (int j = 0; j <= y_size; ++j) {
-    for (int i = 0; i <= x_size; ++i) {
-      Pois.grid[i][j].rho = 1.;
-      Pois.grid[i][j].T = 1.;
-      Pois.grid[i][j].v = Vector2D<double>{0, 0};
-      Pois.grid[i][j].eq();
-    }
-    Pois.grid[Pois.grid.size()-1][j].rho = 1.;
-    Pois.grid[Pois.grid.size()-1][j].eq();
-  }
-  for (int i = 0; i <= x_size; ++i) {
-    for (int j = 0; j <= y_size; ++j) {
-      if (Pois.grid[i][j].interior) {
-        Pois.grid[i][j].f = Pois.grid[i][j].f_eq;
-      } else {
-        for (size_t k = 0; k < Q; k++) {
-          if (Pois.grid[i][j].w_for_bound_point[k] > 0) {
-            Pois.grid[i][j].f[k] = Pois.grid[i][j].f_eq[k];
-          } else {
-            Pois.grid[i][j].f[k] = 0.;
-          }
+        for (size_t i = 0; i <= x_size; ++i) {
+            Pois.grid[i][j].rho = 1.;
+            Pois.grid[i][j].T = 1.;
+            Pois.grid[i][j].v = Vector2D<double>{0, 0};
+            Pois.grid[i][j].eq();
         }
-      }
-      for (size_t k = 0; k < Q; k++) {
-        Pois.grid[i][j].f_temp[k] = 0.;
-      }
+        Pois.grid[Pois.grid.size() - 1][j].rho = 1.;
+        Pois.grid[Pois.grid.size() - 1][j].eq();
     }
-  }
-  for (size_t t = 0; t < iterations; ++t) {
-    for (int i = 0; i <= x_size; ++i) {
-      for (int j = 0; j <= y_size; ++j) {
-        Pois.transfer(i, j);
-      }
-    }
-    Pois.open_boundaries();
-    for (int i = 0; i <= x_size; ++i) {
-      for (int j = 0; j <= y_size; ++j) {
-        if (Pois.grid[i][j].interior) {
-          Pois.grid[i][j].macro();
-          Pois.grid[i][j].eq();
-          Pois.grid[i][j].k_rel = Pois.grid[i][j].k_rel_calculate(y_size);
-          Pois.grid[i][j].tau = Pois.grid[i][j].tau_calculate(y_size);
-          Pois.grid[i][j].col();
+    for (size_t i = 0; i <= x_size; ++i) {
+        for (size_t j = 0; j <= y_size; ++j) {
+            if (Pois.grid[i][j].interior) {
+                Pois.grid[i][j].f = Pois.grid[i][j].f_eq;
+            } else {
+                for (size_t k = 0; k < Q; k++) {
+                    if (Pois.grid[i][j].w_for_bound_point[k] > 0) {
+                        Pois.grid[i][j].f[k] = Pois.grid[i][j].f_eq[k];
+                    } else {
+                        Pois.grid[i][j].f[k] = 0.;
+                    }
+                }
+            }
+            for (size_t k = 0; k < Q; k++) {
+                Pois.grid[i][j].f_temp[k] = 0.;
+            }
         }
-        if (Pois.grid[i][j].bound || Pois.grid[i][j].open_bound) {
-          Pois.grid[i][j].col_for_bound();
+    }
+    for (size_t t = 0; t < iterations; ++t) {
+        for (size_t i = 0; i <= x_size; ++i) {
+            for (size_t j = 0; j <= y_size; ++j) {
+                Pois.transfer(i, j);
+            }
         }
-        Pois.grid[i][j].zeroing_temp();
-      }
+        Pois.open_boundaries();
+        for (size_t i = 0; i <= x_size; ++i) {
+            for (size_t j = 0; j <= y_size; ++j) {
+                if (Pois.grid[i][j].interior) {
+                    Pois.grid[i][j].macro();
+                    Pois.grid[i][j].eq();
+                    Pois.grid[i][j].k_rel = Pois.grid[i][j].k_rel_calculate(y_size);
+                    Pois.grid[i][j].tau = Pois.grid[i][j].tau_calculate(y_size);
+                    Pois.grid[i][j].col();
+                }
+                if (Pois.grid[i][j].bound || Pois.grid[i][j].open_bound) {
+                    Pois.grid[i][j].col_for_bound();
+                }
+                Pois.grid[i][j].zeroing_temp();
+            }
+        }
+        if (t % 100 == 0) {
+            ExportToVtk(Pois.grid, x_size + 1, y_size + 1, t);
+        }
+        if (t % 1000 == 0) {
+            double mass_flow = 0;
+            for (size_t j = 1; j < y_size; ++j) {
+                mass_flow += Pois.grid[50][j].rho * Pois.grid[50][j].v.x;
+            }
+            std::cout << mass_flow << std::endl;
+        }
     }
-    if (t % 100 == 0) {
-      ExportToVtk(Pois.grid, x_size + 1, y_size + 1, t);
-    }
-    if (t % 1000 == 0) {
-      double mass_flow = 0;
-      for (int j = 1; j < y_size; ++j) {
-      mass_flow += Pois.grid[50][j].rho * Pois.grid[50][j].v.x;
-      }
-      std::cout << mass_flow << std::endl;
-    }
-  }
 
-  std::ofstream out;
-  out.open("results.dat");
-  if (out.is_open()) {
-    for (int i = 0; i <= x_size; ++i) {
-      for (int j = 0; j <= y_size; ++j) {
-        out << i << " " << j << " " << Pois.grid[i][j].f << " "
-            << Pois.grid[i][j].rho << " " << Pois.grid[i][j].T << " "
-            << Pois.grid[i][j].v.x << " " << Pois.grid[i][j].v.y << std::endl;
-      }
-    }
-  }
-  out.close();
-  std::ofstream out1;
-  out1.open("results1.dat");
-  if (out1.is_open()) {
-    for (int i = 0; i <= x_size; ++i) {
-      for (int j = 0; j <= y_size; ++j) {
-        if (Pois.grid[i][j].open_bound) {
-          out1 << i << " " << j << " " << Pois.grid[i][j].w_for_bound_point[0]
-               << " " << Pois.grid[i][j].w_for_bound_point[1] << " "
-               << Pois.grid[i][j].w_for_bound_point[2] << " "
-               << Pois.grid[i][j].w_for_bound_point[3] << " "
-               << Pois.grid[i][j].w_for_bound_point[4] << " "
-               << Pois.grid[i][j].w_for_bound_point[5] << " "
-               << Pois.grid[i][j].w_for_bound_point[6] << " "
-               << Pois.grid[i][j].w_for_bound_point[7] << " "
-               << Pois.grid[i][j].w_for_bound_point[8];
-          out1 << std::endl;
+    std::ofstream out;
+    out.open("results.dat");
+    if (out.is_open()) {
+        for (size_t i = 0; i <= x_size; ++i) {
+            for (size_t j = 0; j <= y_size; ++j) {
+                out << i << " " << j << " " << Pois.grid[i][j].f << " "
+                    << Pois.grid[i][j].rho << " " << Pois.grid[i][j].T << " "
+                    << Pois.grid[i][j].v.x << " " << Pois.grid[i][j].v.y << std::endl;
+            }
         }
-      }
     }
-  }
-  out1.close();
+    out.close();
+    std::ofstream out1;
+    out1.open("results1.dat");
+    if (out1.is_open()) {
+        for (size_t i = 0; i <= x_size; ++i) {
+            for (size_t j = 0; j <= y_size; ++j) {
+                if (Pois.grid[i][j].open_bound) {
+                    out1 << i << " " << j << " " << Pois.grid[i][j].w_for_bound_point[0]
+                         << " " << Pois.grid[i][j].w_for_bound_point[1] << " "
+                         << Pois.grid[i][j].w_for_bound_point[2] << " "
+                         << Pois.grid[i][j].w_for_bound_point[3] << " "
+                         << Pois.grid[i][j].w_for_bound_point[4] << " "
+                         << Pois.grid[i][j].w_for_bound_point[5] << " "
+                         << Pois.grid[i][j].w_for_bound_point[6] << " "
+                         << Pois.grid[i][j].w_for_bound_point[7] << " "
+                         << Pois.grid[i][j].w_for_bound_point[8];
+                    out1 << std::endl;
+                }
+            }
+        }
+    }
+    out1.close();
 
-  return 0;
+    return 0;
 }
--- vector_2d.h
+++ vector_2d.h
@@ -5,35 +5,49 @@
 #ifndef LBM_CPP_VECTOR_2D_H
 #define LBM_CPP_VECTOR_2D_H
 
-template <typename T> struct Vector2D {
-  T x;
-  T y;
-  explicit Vector2D(T x = 0, T y = 0) : x(x), y(y) {}
-  T operator*(const Vector2D<T> &rhs) const {
+template<typename T>
+struct Vector2D {
+    T x;
+    T y;
 
-    return this->x * rhs.x + this->y * rhs.y;
-  }
-  Vector2D operator+=(const Vector2D<T> &rhs) {
-    this->x += rhs.x;
-    this->y += rhs.y;
-    return *this;
-  }
-  friend Vector2D operator+(Vector2D<T> lhs, const Vector2D<T> &rhs) {
-    lhs += rhs;
-    return lhs;
-  }
-  Vector2D operator-=(const Vector2D<T> &rhs) {
-    this->x -= rhs.x;
-    this->y -= rhs.y;
-    return *this;
-  }
-  friend Vector2D operator-(Vector2D<T> lhs, const Vector2D<T> &rhs) {
-    lhs -= rhs;
-    return lhs;
-  }
-  friend Vector2D operator*(T scalar, const Vector2D<T> &rhs) {
-    return Vector2D(scalar * rhs.x, scalar * rhs.y);
-  }
+    explicit Vector2D(T x_ = 0, T y_ = 0)
+    : x(x_)
+    , y(y_) {}
+
+    T operator*(const Vector2D<T> &rhs) const {
+
+        return this->x * rhs.x + this->y * rhs.y;
+    }
+
+    Vector2D operator+=(const Vector2D<T> &rhs) {
+        this->x += rhs.x;
+        this->y += rhs.y;
+        return *this;
+    }
+
+    friend Vector2D operator+(Vector2D<T> lhs, const Vector2D<T> &rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+
+    Vector2D operator-=(const Vector2D<T> &rhs) {
+        this->x -= rhs.x;
+        this->y -= rhs.y;
+        return *this;
+    }
+
+    friend Vector2D operator-(Vector2D<T> lhs, const Vector2D<T> &rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+
+    friend Vector2D operator*(T scalar, const Vector2D<T> &rhs) {
+        return Vector2D(scalar * rhs.x, scalar * rhs.y);
+    }
+
+    friend bool operator==(const Vector2D<T> &lhs, const Vector2D<T> &rhs) {
+        return lhs.x == rhs.x && lhs.y == rhs.y;
+    }
 };
 
 #endif // LBM_CPP_VECTOR_2D_H
